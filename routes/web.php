fcc461ca57efafc7a7c2c4e1a9bb6a344517b3a2
afdhaladal <afdhal22ti@mahasiswa.pcr.ac.id>
2024-07-07 16:12:07 +0700

    afdhal

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\LayoutController; 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::redirect('/', [LayoutController::class, 'index']);
+Route::redirect('/', '/dashboard');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -15,6 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Tambahkan rute untuk LayoutController
 });
+Route::get('/layout', [LayoutController::class, 'index'])->name('layout.index');
+
 
 require __DIR__.'/auth.php';
