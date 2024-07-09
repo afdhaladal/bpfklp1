b42353f2fdd78a5ce70dad20d120164a72ccd111
Qafabil <qafabil22ti@mahasiswa.pcr.ac.id>
2024-07-09 22:22:50 +0700

    qafabil

---
+++ database/factories/InfoFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Info>
+ */
+class InfoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'image_path' => fake()->imageUrl(),
+            'created_by' => 1,
+            'updated_by' => 1
+        ];
+    }
+}
--- database/migrations/2024_07_09_150435_create_infos_table.php
+++ database/migrations/2024_07_09_150435_create_infos_table.php
@@ -4,18 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('contents', function (Blueprint $table) {
+        Schema::create('infos', function (Blueprint $table) {
             $table->id();
             $table->string('image_path')->nullable();
-
-            $table->foreignId('created_by')->constrained('contents');
-            $table->foreignId('updated_by')->constrained('contents');
+            $table->foreignId('created_by')->constrained('infos');
+            $table->foreignId('updated_by')->constrained('infos');
+            $table->timestamps();
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('contents');
+        Schema::dropIfExists('infos');
     }
 };
